72edd058b76f599ab62e213854c181dfbf097f00
Chatresh <chatreshrsg0202@gmail.com>
2022-04-12 10:35:19 +0530

    Books fetching fixed

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function switchTheme(){
     let root = document.querySelector(":root");
-    
     let rs = getComputedStyle(root);
-    console.log(rs.getPropertyValue('--theme'));
     if (rs.getPropertyValue('--theme') == "white"){
         root.style.setProperty('--theme', 'rgb(42, 41, 48)');
     }
